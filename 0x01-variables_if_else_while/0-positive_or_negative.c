b23ed88c2da580c0185b5afe28796167114de3a2
Olive40 <olivemkalonga@gmail.com>
2023-07-19 02:10:47 -0700

    1-last_digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,21 +9,21 @@
  */
 int main(void)
 {
-	int n:
+	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX \ 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("%d is positive\n". n);
+		printf("is positive\n");
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("is zero\n");
 	}
 	else
 	{
-	printf("%d is negative\n");
+		printf("is negative\n");
 	}
-	return (0)
+	return (0);
 }
